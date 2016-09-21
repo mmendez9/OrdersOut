848b702a5914c13dc582a0ad63eec7b569b57b43
mmendez9 <hikari1995>
2016-09-21 10:54:52 -0400

    Orders

--- OrdersOut/Form1.Designer.cs
+++ OrdersOut/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbordersout = new System.Windows.Forms.ListBox();
+            this.clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbordersout
+            // 
+            this.lbordersout.FormattingEnabled = true;
+            this.lbordersout.Location = new System.Drawing.Point(32, 22);
+            this.lbordersout.Name = "lbordersout";
+            this.lbordersout.Size = new System.Drawing.Size(209, 173);
+            this.lbordersout.TabIndex = 0;
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(104, 216);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(81, 34);
+            this.clear.TabIndex = 1;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.lbordersout);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lbordersout;
+        private System.Windows.Forms.Button clear;
     }
 }
 
--- OrdersOut/Form1.cs
+++ OrdersOut/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,49 @@ namespace OrdersOut
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string orders;
+                StreamReader order = File.OpenText("Z:/Visual Studio 2015/Projects/7Sept16-1/7Sept16-1/bin/Debug/Orders.txt");
+
+                while (!order.EndOfStream)
+                {
+                    // read line in the text file
+                    orders = order.ReadLine();
+
+                    // Add the items to the list box
+                    lbordersout.Items.Add(orders);
+                }
+                order.Close();
+            }
+            catch (Exception ex)
+            {
+                // Error open the file
+                MessageBox.Show(ex.Message, "Ice Cream", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            StreamWriter orderfile = File.CreateText
+                ("Z:/Visual Studio 2015/Projects/7Sept16-1/7Sept16-1/bin/Debug/Orders.txt");
+
+            DialogResult result = MessageBox.Show("Are you sure to CLEAR the orders file?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                
+                // Delete content of the file
+                orderfile.WriteLine("");
+                orderfile.Close();
+
+                lbordersout.Items.Clear();
+            }
         }
     }
 }
